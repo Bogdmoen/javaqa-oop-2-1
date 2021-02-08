d7c13aafa51bbb0f5c9d2b004cabcd6a4bef2add
Bogdan Klimchuk <bogd.moen@yandex.com>
2021-02-08 03:33:29 +0300

    object updated

--- src/main/java/ru/netology/domain/VkPost.java
+++ src/main/java/ru/netology/domain/VkPost.java
@@ -4,13 +4,16 @@ public class VkPost {
 
     private String id;  // уникальный идентификатор обьекта
     private String date; // дата и время публикации
+    private String creatorId; // идентификатор создателя поста
     private String text; // текст записи
     private int likeCounter; // счетчик количества лайков
+    private boolean isLiked; // Информация о том, поставил ли текущий пользователь лайк посту
     private int commentCounter; // счетчик количества комментариев
     private  int repostCounter; // счетчик количества репостов
     private int viewsCounter; // счетчик колчиества простотор поста
     private boolean isCommentAllowed; // включает \ отключает возможность комментировать запись
+    private boolean isRepostAllowed;  // включает \ отключает возможность делать репост записи
     private boolean isAd; // содержит информацию является ли запись рекламой
     private int attachments []; // содержит список id файлов, добавленных в пост
-    private String creatorId; // идентификатор создателя поста
+
 }
